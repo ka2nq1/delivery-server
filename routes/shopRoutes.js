fa23eea8da5582abb9272a417f7c703b4a8f0412
Aleksandr Vlasiuk <70466181+ka2nq1@users.noreply.github.com>
2023-06-08 21:44:11 +0300

    Update shopRoutes.js

--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Shop = require('../models/Shop');
-const { v4: uuidv4 } = require('uuid');
 
 router.get('/', async (req, res) => {
     try {
         const shops = await Shop.find();
-        res.json({ shops, sessionId: uuidv4() });
+        res.json({ shops );
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Server error' });
